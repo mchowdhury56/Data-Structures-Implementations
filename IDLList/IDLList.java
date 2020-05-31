8d1a2dee2b5fe12cbd1fe80498e514914386f632
Marjan Chowdhury <marjanchowdhury7@gmail.com>
2020-05-30 23:31:43 -0400

    Implementation of a doubly linked list that uses an ArrayList for fast indexing

--- IDLList/IDLList.java
+++ IDLList/IDLList.java
@@ -1,13 +1,12 @@
 package IDLList;
 import java.util.ArrayList;
-
-
 /**
  * This class creates a doubly linked list with fast indexing
  * @author MarjanChowdhury
  */
 
-public class IDLList<E> {
+
+ public class IDLList<E> {
 	
 	/**
 	 * This class creates the nodes used in linked lists
